1a408443bb7911b4fe943d9a77dff9453e082b22
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-10-16 22:13:42 +0100

    Matrix header and source files implemented into the solution.

---
+++ DX11 Framework 2018/utility/Matrix.cpp
@@ -0,0 +1,258 @@
+#ifndef __MATRIX_CPP
+#define __MATIRX_CPP
+
+#include "Matrix.h"
+
+// parameter constructor
+template<typename T>
+Matrix<T>::Matrix( unsigned int rows, unsigned int cols, const T& initial )
+{
+	this->matrix.resize( rows );
+	for ( int i = 0; i < this->matrix.size(); i++ )
+		this->matrix[i].resize( cols, initial );
+
+	this->rows = rows;
+	this->cols = cols;
+}
+
+// copy constructor
+template<typename T>
+Matrix<T>::Matrix( const Matrix<T>& rhs )
+{
+	this->matrix = rhs.matrix;
+	this->rows = rhs.GetRows();
+	this->cols = rhs.GetCols();
+}
+
+// virtual destructor
+template<typename T>
+Matrix<T>::~Matrix() { }
+
+// assignment operator
+template<typename T>
+Matrix<T>& Matrix<T>::operator=( const Matrix<T>&  rhs )
+{
+	if ( &rhs == this )
+		return *this;
+
+	unsigned int newRows = rhs.GetRows();
+	unsigned int newCols = rhs.GetCols();
+
+	this->matrix.resize( newRows );
+	for ( int i = 0; i < this->matrix.size(); i++ )
+		this->matrix[i].resize( newCols );
+
+	for ( int i = 0; i < newRows; i++ )
+	{
+		for ( int j = 0; j < newCols; j++ )
+			this->matrix[i][j] = rhs( i, j );
+	}
+
+	this->rows = newRows;
+	this->cols = newCols;
+
+	return *this;
+}
+
+// matrix addition
+template<typename T>
+Matrix<T> Matrix<T>::operator+( const Matrix<T>& rhs )
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	for ( int i = 0; i < this->rows; i++ )
+	{
+		for ( int j = 0; j < this->cols; j++ )
+			result( i, j ) = this->matrix[i][j] + rhs( i, j );
+	}
+	return result;
+}
+
+// cumulative adddition of 'this' matrix and another
+template<typename T>
+Matrix<T>& Matrix<T>::operator+=( const Matrix<T>& rhs )
+{
+	unsigned int rows = rhs.GetCols();
+	unsigned int cols = rhs.GetRows();
+
+	for ( int i = 0; i < rows; i ++ )
+	{
+		for ( int j = 0; j < cols; j++ )
+			this->matrix[i][j] += rhs( i, j );
+	}
+
+	return *this;
+}
+
+// subtraction of 'this' matrix and another
+template<typename T>
+Matrix<T> Matrix<T>::operator-( const Matrix<T>& rhs )
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	unsigned int rows = rhs.GetRows();
+	unsigned int cols = rhs.GetCols();
+	for ( int i = 0; i < rows; i++ )
+	{
+		for ( int j = 0; j < cols; j++ )
+			result( i, j ) = this->matrix[i][j] - rhs( i, j );
+	}
+	return result;
+}
+
+// cumulative subtraction of 'this' matrix and another
+template<typename T>
+Matrix<T>& Matrix<T>::operator-=( const Matrix<T>& rhs )
+{
+	unsigned int rows = rhs.GetRows();
+	unsigned int cols = rhs.GetCols();
+	for ( int i = 0; i < rows; i++ )
+	{
+		for ( int j = 0; j < cols; j++ )
+			this->matrix[i][j] -= rhs( i, j );
+	}
+	return *this;
+}
+
+// left multiplication of 'this' matrix and another
+template<typename T>
+Matrix<T> Matrix<T>::operator*( const Matrix<T>& rhs )
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	unsigned int rows = rhs.GetRows();
+	unsigned int cols = rhs.GetCols();
+	for ( int i = 0; i < rows; i++ )
+	{
+		for ( int j = 0; j < cols; j++ )
+		{
+			for ( int k = 0; k < rows, k++ )
+				result( i, j ) += this->matrix[i][k] * rhs( k, j );
+		}
+	}
+	return result;
+}
+
+// cumulative left multiplication of 'this' matrix and another
+template<typename T>
+Matrix<T>& Matrix<T>::operator*=( const Matrix<T>& rhs )
+{
+	Matrix result = ( *this ) * rhs;
+	( *this ) = result;
+	return *this;
+}
+
+// calculate the transpose of 'this' matrix
+template<typename T>
+Matrix<T> Matrix<T>::Transpose()
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	for ( int i = 0; i < this->rows; i++ )
+	{
+		for ( int j = 0; j < this->cols; j++ )
+			result( i, j ) = this->matrix[j][i];
+	}
+	return this->matrix;
+}
+
+// matrix / scalar addition
+template<typename T>
+Matrix<T> Matrix<T>::operator+( const T& rhs )
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	for ( int i = 0; i < this->rows; i++  )
+	{
+		for ( int j = 0; j < this->cols; j++ )
+			result( i, j ) = this->matrix[i][j] + rhs;
+	}
+	return result;
+}
+
+// matrix / scalar subtraction
+template<typename T>
+Matrix<T> Matrix<T>::operator-( const T& rhs )
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	for ( int i = 0; i < this->rows; i++  )
+	{
+		for ( int j = 0; j < this->rows; j++ )
+			result( i, j ) = this->matrix[i][j] - rhs;
+	}
+	return result;
+}
+
+// matrix / scalar multiplication
+template<typename T>
+Matrix<T> Matrix<T>::operator*( const T& rhs )
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	for ( int i = 0; i < this->rows; i++  )
+	{
+		for ( int j = 0; j < this->rows; j++ )
+			result( i, j ) = this->matrix[i][j] * rhs;
+	}
+	return result;
+}
+
+// matrix / scalar division
+template<typename T>
+Matrix<T> Matrix<T>::operator/( const T& rhs )
+{
+	Matrix result( this->rows, this->cols, 0.0 );
+	for ( int i = 0; i < this->rows; i++  )
+	{
+		for ( int j = 0; j < this->rows; j++ )
+			result( i, j ) = this->matrix[i][j] / rhs;
+	}
+	return result;
+}
+
+// multiply a matrix with a vector
+template<typename T>
+std::vector<T> Matrix<T>::operator*( const std::vector<T>& rhs )
+{
+	std::vector<T> result( rhs.size(), 0.0 );
+	for ( int i = 0; i < this->row; i++ )
+	{
+		for ( int j = 0; j < this->cols; j++ )
+			result[i] += this->matrix[i][j] * rhs[j];
+	}
+	return result;
+}
+
+// get vector of the diagonal elements
+template<typename T>
+std::vector<T> Matrix<T>::DiagonalVector()
+{
+	std::vector<T> result( this->rows, 0.0 );
+	for ( int i = 0; i < this->rows; i++ )
+		diagonalElements[i] = this->matrix[i][i];
+	return result;
+}
+
+// access the individual elements
+template<typename T>
+T& Matrix<T>::operator() ( const unsigned int& row, const unsigned int& col )
+{
+	return this->matrix[row][col];
+}
+
+// access the individual elements - const
+template<typename T>
+const T& Matrix<T>::operator() ( const unsigned int& row, const unsigned int& col ) const
+{
+	return this->matrix[row][col];
+}
+
+// get the number of rows in the matrix
+template<typename T>
+unsigned int Matrix<T>::GetRows() const
+{
+	return this->rows;
+}
+
+// get the number of cols in the matrix
+template<typename T>
+unsigned int Matrix<T>::GetCols() const
+{
+	return this->cols;
+}
+
+#endif
---
+++ DX11 Framework 2018/utility/Matrix.h
@@ -0,0 +1,47 @@
+#ifndef __MATRIX_H
+#define __MATRIX_H
+
+//#pragma once
+#include <vector>
+
+template<typename T>
+class Matrix
+{
+public:
+	Matrix( unsigned int rows, unsigned int cols, const T& initial );
+	Matrix( const Matrix<T>& rhs );
+	virtual ~Matrix();
+public:
+	// operator overloading
+	Matrix<T>& operator=( const Matrix<T>& rhs );
+	// matrix mathematical operations
+	Matrix<T> operator+( const Matrix<T>& rhs );
+	Matrix<T>& operator+=( const Matrix<T>& rhs );
+	Matrix<T> operator-( const Matrix<T>& rhs );
+	Matrix<T>& operator-=( const Matrix<T>& rhs );
+	Matrix<T> operator*( const Matrix<T>& rhs );
+	Matrix<T>& operator*=( const Matrix<T>& rhs );
+	Matrix<T> Transpose();
+public:
+	// matrix scalar operations
+	Matrix<T> operator+( const T& rhs );
+	Matrix<T> operator-( const T& rhs );
+	Matrix<T> operator*( const T& rhs );
+	Matrix<T> operator/( const T& rhs );
+public:
+	// matrix / vector operations
+	std::vector<T> operator*( const std::vector<T>& rhs );
+	std::vector<T> DiagonalVector();
+	// access the individual elements
+	T& operator() ( const unsigned int& row, const unsigned int& col );
+	const T& operator() ( const unsigned int& row, const unsigned int& col ) const;
+	// access the row and column sizes
+	unsigned GetRows() const;
+	unsigned GetCols() const;
+private:
+	std::vector<std::vector<T>> matrix;
+	unsigned int rows, cols;
+};
+
+//#include "Matrix.cpp"
+#endif
