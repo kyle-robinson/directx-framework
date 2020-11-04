595866ced4e7bbbfb6bea68f9a5b9875ce794916
Kyle Robinson <w021256j@student.staffs.ac.uk>
2020-11-04 00:09:47 +0000

    Cleaned up code in Graphics header file.

--- DX11 Framework 2018/graphics/Graphics.h
+++ DX11 Framework 2018/graphics/Graphics.h
@@ -35,30 +35,7 @@ public:
 	void Update( float dt );
 	UINT GetWidth() const noexcept;
 	UINT GetHeight() const noexcept;
-private:
-	bool InitializeDirectX( HWND hWnd );
-	bool InitializeShaders();
-	bool InitializeScene();
-private:
-	Microsoft::WRL::ComPtr<ID3D11Device> device;
-	Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain;
-	Microsoft::WRL::ComPtr<ID3D11DeviceContext> context;
-	
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> boxTexture;
-	//Microsoft::WRL::ComPtr<ID3D11RenderTargetView> backBuffer;
-	//Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView;
-	//Microsoft::WRL::ComPtr<ID3D11RenderTargetView> nullRenderTarget = nullptr;
-	//Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> nullShaderResourceView = nullptr;
-	//Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceView;
-	
-	std::shared_ptr<Bind::Blender> blendState;
-	std::shared_ptr<Bind::DepthStencil> depthStencil;
-	std::shared_ptr<Bind::RenderTarget> backBuffer;
-	std::shared_ptr<Bind::RenderTarget> renderTarget;
-	std::map<std::string, std::shared_ptr<Bind::Sampler>> samplerStates;
-	std::map<std::string, std::shared_ptr<Bind::Stencil>> stencilStates;
-	std::map<std::string, std::shared_ptr<Bind::Rasterizer>> rasterizerStates;
-public:
+
 	Light light;
 	Sprite circle;
 	Sprite square;
@@ -67,6 +44,23 @@ public:
 	RenderableGameObject nanosuit;
 	ConstantBuffer<CB_PS_light> cb_ps_light;
 private:
+	bool InitializeDirectX( HWND hWnd );
+	bool InitializeShaders();
+	bool InitializeScene();
+
+	Microsoft::WRL::ComPtr<ID3D11Device> device;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> context;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> boxTexture;
+
+	std::shared_ptr<Bind::Blender> blendState;
+	std::shared_ptr<Bind::DepthStencil> depthStencil;
+	std::shared_ptr<Bind::RenderTarget> backBuffer;
+	std::shared_ptr<Bind::RenderTarget> renderTarget;
+	std::map<std::string, std::shared_ptr<Bind::Sampler>> samplerStates;
+	std::map<std::string, std::shared_ptr<Bind::Stencil>> stencilStates;
+	std::map<std::string, std::shared_ptr<Bind::Rasterizer>> rasterizerStates;
+
 	VertexShader vertexShader_2D;
 	VertexShader vertexShader_full;
 	VertexShader vertexShader_light;
@@ -88,7 +82,7 @@ private:
 	ConstantBuffer<CB_VS_matrix> cb_vs_matrix;
 	ConstantBuffer<CB_VS_matrix_2D> cb_vs_matrix_2d;
 	ConstantBuffer<CB_VS_fullscreen> cb_vs_fullscreen;
-private:
+
 	UINT windowWidth;
 	UINT windowHeight;
 	ImGuiManager imgui;
