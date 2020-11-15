c3c859b14cdcfd8fb18a6a270b841540edcb26b0
Kyle ROBINSON <w021256j@student.staffs.ac.uk>
2020-11-15 19:49:25 +0000

    Updated scene controls in main ImGui window.

--- DX11 Framework 2018/graphics/ImGuiManager.cpp
+++ DX11 Framework 2018/graphics/ImGuiManager.cpp
@@ -71,14 +71,27 @@ void ImGuiManager::RenderMainWindow( Graphics& gfx )
                 ImGui::Text( "SPACE\t->\tUpward" );
                 ImGui::Text( "SHIFT\t->\tMove Faster" );
                 ImGui::Text( "RMB  \t->\tRotate Camera" );
+                ImGui::Text( "1    \t->\tSwitch to Camera 1" );
+                ImGui::Text( "2    \t->\tSwitch to Camera 2" );
+                ImGui::Text( "3    \t->\tSwitch to Camera 3" );
                 ImGui::PopStyleColor();
                 ImGui::TreePop();
             }
-            
             if ( ImGui::TreeNode( "Light Controls" ) )
             {
                 ImGui::PushStyleColor( ImGuiCol_Text, { 1.0f, 1.0f, 1.0f, 1.0f } );
-                ImGui::Text( "C\t\t->\tMove Light to Camera" );
+                ImGui::Text( "C\t\t->\tStick Light to Camera" );
+                ImGui::Text( "X\t\t->\tUnstick Light from Camera" );
+                ImGui::PopStyleColor();
+                ImGui::TreePop();
+            }
+            if ( ImGui::TreeNode( "Viewport Controls" ) )
+            {
+                ImGui::PushStyleColor( ImGuiCol_Text, { 1.0f, 1.0f, 1.0f, 1.0f } );
+                ImGui::Text( "Up   \t->\tFullscreen viewport" );
+                ImGui::Text( "Left \t->\tLeft viewport only" );
+                ImGui::Text( "Right\t->\tRight viewport only" );
+                ImGui::Text( "Down \t->\tSplit-screen mode" );
                 ImGui::PopStyleColor();
                 ImGui::TreePop();
             }
